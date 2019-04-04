2b98535dbc686a33effc65eadd29b519fff750ba
Zafar Ahmad <zahmad@iesjoandaustria.org>
2019-04-04 20:53:16 +0200

    Conection with DataBase PostgreSQL

---
+++ src/Servlet/BBDD.java
@@ -0,0 +1,58 @@
+package Servlet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * @author imssbora
+ */
+public class BBDD {
+  public static void main(String[] args) {
+
+    String jdbcUrl = "jdbc:postgresql://localhost:5432/JavaTomcat";
+    String username = "postgres";
+    String password = "123";
+
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    try {
+        // Step 1 - Load driver
+       // Class.forName("org.postgresql.Driver"); // Class.forName() is not needed since JDBC 4.0
+
+        // Step 2 - Open connection
+        conn = DriverManager.getConnection(jdbcUrl, username, password);
+
+        // Step 3 - Execute statement
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery("SELECT * from users");
+
+        // Step 4 - Get result
+
+        if (rs.next()) {
+          System.out.println(rs.getString(1));
+        }
+
+      } catch (SQLException e) {
+        e.printStackTrace();
+      } 
+        
+  	}
+}
+
+
+
+
--- src/Zafar/LoginCase.java
+++ src/Zafar/LoginCase.java
@@ -1,9 +1,11 @@
 package Zafar;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginCase {
 
@@ -11,40 +13,49 @@ public class LoginCase {
 
 	/*CREATE TABLE*/
 	public static boolean insertUser(String uName, String uPass, String uEmail) {
-		Connection conectionNick;
-		
-		PreparedStatement pss = null;
-		PreparedStatement nick = null;
-		
-		
-		try {
-			String consultaNick = "select * from users where nick=?;";
-			
-			nick.setString(1,uName);
-			ResultSet Respuesta = nick.executeQuery();
-			
-			if (Respuesta.next()) {
-				return false;
-			}else {
-				
-				String consulta = "insert into users (NICK,PASS,EMAIL) VALUES (?,?,?);";
-			
-				pss.setString(1,uName);
-				pss.setString(2,uPass);
-				pss.setString(3,uEmail);
-				
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				if (pss != null) {
-					pss.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+	    String jdbcUrl = "jdbc:postgresql://localhost:5432/JavaTomcat";
+	    String username = "postgres";
+	    String password = "123";
+
+	    Connection conn = null;
+	    Statement stmt = null;
+	    ResultSet rs = null;
+
+	    try {
+	        // Step 1 - Load driver
+	       // Class.forName("org.postgresql.Driver"); // Class.forName() is not needed since JDBC 4.0
+
+	        // Step 2 - Open connection
+	        conn = DriverManager.getConnection(jdbcUrl, username, password);
+
+	        // Step 3 - Execute statement
+	        stmt = conn.createStatement();
+	        rs = stmt.executeQuery("insert into users(nick,pass,email) values");
+
+	        // Step 4 - Get result
+	        if (rs.next()) {
+	          System.out.println(rs.getString(1)+" " +rs.getString(2) +" " +rs.getString(3));
+	        }
+
+	      } catch (SQLException e) {
+	        e.printStackTrace();
+	      } finally {
+	        try {
+
+	          // Step 5 Close connection
+	          if (stmt != null) {
+	            stmt.close();
+	          }
+	          if (rs != null) {
+	            rs.close();
+	          }
+	          if (conn != null) {
+	              conn.close();
+	            }
+	          } catch (Exception e) {
+	            e.printStackTrace();
+	          }
+	        }
 		return false;
 	}
 }
